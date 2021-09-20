2b290395d06ad42354be28cf7f127b02a267fb5c
Tanver Hasan <B00316640@gmail.com>
2021-09-20 15:08:13 +0100

    token verification added

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
+require('dotenv').config()
 const express = require("express");
 const path = require("path");
 const _ = require("lodash");
+const jwt = require("jsonwebtoken");
 
 const port = process.env.PORT || 8081;
+const secret = process.env.SECRET;
 
 const app = express();
-app.use(express.json()); 
-app.use(express.urlencoded({extended: true})); 
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.use((req, res, next) => {
   console.log("/" + req.method, req.path);
-  console.log("Authorization Token" +req.headers.authorization);
   next();
 });
 
 app.get("/", (req, res, err) => {
-    res.send("sms-gateway server");
+  res.send("sms-gateway server");
 });
 
 app.post("/sms-gateway", (req, res, err) => {
+ 
+
+  const token = req.headers.authorization || null;
+  if (!token) {
+    throw new Error("Authorization token is required");
+  }
+  console.log("Authorization Token : " + token);
+  
+  if (!secret) {
+    console.log("Secret can not be found");
+    throw new Error("Secret is not provided");
+  }
+  console.log("Secret : "+ secret);
+  try {
+    jwt.verify(token, secret);
+  } catch (error) {
+    throw new Error("Token verification failed : " + error);
+  }
+  console.log("Token Verification successful");
   console.log(req.body);
   res.sendStatus(200);
 });
