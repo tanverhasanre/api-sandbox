d0a3e747898a4482c9806ca6c509f50341c22420
Tanver Hasan <B00316640@gmail.com>
2021-09-20 14:18:53 +0100

    Added a Procfile.

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const path = require("path");
+const _ = require("lodash");
+
+const port = process.env.PORT || 8081;
+
+const app = express();
+app.use(express.json()); 
+app.use(express.urlencoded({extended: true})); 
+
+
+app.use((req, res, next) => {
+  console.log("/" + req.method, req.path);
+  next();
+});
+
+app.get("/", (req, res, err) => {
+    res.send("sms-gateway server");
+});
+
+app.post("/sms-gateway", (req, res, err) => {
+  console.log(req.body);
+  res.sendStatus(200);
+});
+
+app.listen(port, () => console.log(`server is running on ${port}`));
