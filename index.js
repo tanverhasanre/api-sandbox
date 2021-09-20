2a4634691d24290d03b846c93fcd770656d99936
Tanver Hasan <B00316640@gmail.com>
2021-09-20 15:49:41 +0100

    removed debug statement

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
 const _ = require("lodash");
@@ -20,39 +20,41 @@ app.get("/", (req, res, err) => {
   res.send("sms-gateway server");
 });
 
-function getToken (req) {
-    console.log("getToken function called");
-
-    if (req.headers.authorization && req.headers.authorization.split(' ')[0] === 'Bearer') {
-        return req.headers.authorization.split(' ')[1];
-    } else if (req.query && req.query.token) {
-      return req.query.token;
-    }
-    return null;
+function getToken(req) {
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    return req.headers.authorization.split(" ")[1];
+  } else if (req.query && req.query.token) {
+    return req.query.token;
+  }
+  return null;
 }
 
 app.post("/sms-gateway", (req, res, err) => {
- 
-
   const token = getToken(req);
-  console.log("38:Token : "+token);
+
   if (!token) {
     throw new Error("Authorization token is required");
   }
   console.log("Authorization Token : " + token);
-  
+
   if (!secret) {
-    console.log("Secret can not be found");
     throw new Error("Secret is not provided");
   }
-  console.log("Secret : "+ secret);
-  jwt.verify(token,secret,function(err,decoded){
-      if(err){
-          throw new Error("Error : "+ err)
-      }
-      console.log(decoded);
-  })
+  console.log("Secret : " + secret);
+
+  jwt.verify(token, secret, function (err, decoded) {
+    if (err) {
+      throw new Error("Error : " + err);
+    }
+    console.log(decoded);
+  });
   res.sendStatus(200);
 });
 
+(async() => {
+
 app.listen(port, () => console.log(`server is running on ${port}`));
+})();
