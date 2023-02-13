d6819b17cac836d474d3fd0e594cab8269a495eb
Tanver-Hasan <38843176+Tanver-Hasan@users.noreply.github.com>
2023-02-13 15:33:26 +0000

    added proxy endpont

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const express = require("express");
 const path = require("path");
 const _ = require("lodash");
 const jwt = require("jsonwebtoken");
-const { getToken} = require("./utils");
+const querystring = require("querystring");
+
+const { getToken, validateAPIKey } = require("./utils");
 const port = process.env.PORT || 8081;
 const secret = process.env.SECRET;
 
@@ -16,29 +18,60 @@ app.use((req, res, next) => {
   next();
 });
 
-app.get("/test_http",(req,res,err)=>{
-  console.log("Headers : "+ JSON.stringify(req.headers));
-  console.log("Body : "+ JSON.stringify(req.body));
-  const user={
-    name:"abcd",
-    age:20,
+app.get("/test_http", (req, res, err) => {
+  console.log("Headers : " + JSON.stringify(req.headers));
+  console.log("Body : " + JSON.stringify(req.body));
+  const user = {
+    name: "abcd",
+    age: 20,
     kyc: true,
-    phone:"07832953525"
-  }
+    phone: "07832953525",
+  };
   res.status(200).send(user);
 });
 
 app.post("/webhook", (req, res, err) => {
-  console.log("Headers : "+ JSON.stringify(req.headers));
-  console.log("Body : "+ JSON.stringify(req.body));
+  console.log("Headers : " + JSON.stringify(req.headers));
+  console.log("Body : " + JSON.stringify(req.body));
   res.sendStatus(200);
 });
 
-app.post("/sms-gateway", (req, res, err) => {
+app.post("/proxy", (req, res, err) => {
+  console.log("Headers : " + JSON.stringify(req.headers));
+  console.log("Body : " + JSON.stringify(req.body));
 
-  console.log("Headers : "+ JSON.stringify(req.headers));
-  console.log("Body : "+ JSON.stringify(req.body));
-  
+  const parsed = querystring.parse(location.search);
+  const appUrl=parsed.destination_url;
+  const state= parsed.state;
+  const code=parsed.code; 
+  const url = `${appUrl}?code=${code}&state=${state}`;
+
+  res.redirect(url);
+});
+
+
+
+app.post("/okta-eventhook", (req, res, err) => {
+  console.log("Headers : " + JSON.stringify(req.headers));
+  console.log("Body : " + JSON.stringify(req.body));
+
+  const api_key = req.headers.authorization || "";
+  // checking api key is not null
+  if (!api_key) {
+    res.sendStatus(401);
+  }
+  const validation = validateAPIKey(api_key);
+  if (validation) {
+    // execte business logic
+    
+    res.sendStatus(200);
+  }
+});
+
+app.post("/sms-gateway", (req, res, err) => {
+  console.log("Headers : " + JSON.stringify(req.headers));
+  console.log("Body : " + JSON.stringify(req.body));
+
   const token = getToken(req);
 
   if (!token) {
@@ -50,7 +83,7 @@ app.post("/sms-gateway", (req, res, err) => {
     throw new Error("Secret is not provided");
   }
   console.log("Secret : " + secret);
-  
+
   jwt.verify(token, secret, function (err, decoded) {
     if (err) {
       throw new Error("Error : " + err);
--- utils.js
+++ utils.js
@@ -1,15 +1,25 @@
 function getToken(req) {
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.split(" ")[0] === "Bearer"
-    ) {
-      return req.headers.authorization.split(" ")[1];
-    } else if (req.query && req.query.token) {
-      return req.query.token;
-    }
-    return null;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.split(" ")[0] === "Bearer"
+  ) {
+    return req.headers.authorization.split(" ")[1];
+  } else if (req.query && req.query.token) {
+    return req.query.token;
   }
+  return null;
+}
 
-  module.exports={
-      getToken
+function validateAPIKey(api_key) {
+  // checking if API key is valid
+  if (_isEqual(api_key, process.env.api_key)) {
+    // chek if the token is reovked (optional)
+    return true;
   }
+  return false;
+}
+
+module.exports = {
+  getToken,
+  validateAPIKey,
+};
